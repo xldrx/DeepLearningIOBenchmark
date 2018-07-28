944e3faa0919411d66400a918df4850ed1f23080
Sayed Hadi Hashemi <SayedHadiHashemi@gmail.com>
2018-07-27 22:59:37 -0700

    Change layout

--- README.md
+++ README.md
@@ -2,18 +2,88 @@
 The goal of this project is to simulate a deep learning training job load on a storage device.
 
 ## Requirements
-This project relies on `fio`.
+* `fio`. It has been tested on `3.8`.
+* `Python 3`. Tested on `3.6`.
+* packages in `requirements.txt`. Install by:
+```bash
+pip3 install -r requirements.txt
+```
+
 
 ## Run
-1. 
+1. choose a set of API and workload.
+2. Set `DEVICE` environment variable to the target mount point.
+```bash
+export DEVICE=/mnt/target
+```
+3. Run the `fio`:
+```bash
+fio --output-format=json --output=OUTPUT experiments/API/WORKLOAD.fio
+```
+where `API` is the chosen API, `WORKLOAD` is the workload, and `OUTPUT` is the output file.
+for example for `sync` API and `imagenet` workload run:
+```bash
+fio --output-format=json --output=results.json experiments/sync/imagenet.fio
+```
 
-## Benchmarks
-1. **Microbenchmarks**: This benchmark  
-2. **Imagenet**: This benchmark simulate reading 
-3. **Imagenet Sequence Files**: 
+## API sets
+There are 4 different API sets for the benchmarks.
+While all sets simulate the same workload, they use different kernel calls for reading the data.
+Some APIs may not be supported on some systems. 
+* `sync`: Use the standard `read` API. It is compatible with all settings.
+* `async`: This uses `AIO` library. Usually it provides better throughput in exchange to worst latency. It may not be supported on some platforms.
+* `sync-direct`: It uses the same API as `sync` but bypass the kernel. In theory, it should have better performance, but may not be fully supported in some settings. 
+* `async-indirect`: It uses the same API as `async` but *does not* bypass the kernel. In theory, it should have worse performance than `async` but supports more settings. 
 
-## How does it work?
-TODO
+## Workloads
+1. **General**: This workload simulates random read of a fixed size buffer.
+2. **Dataset**: This workload simulates a random read of examples of different sizes following the distribution of a dataset. Additionally, this workload mimicking the behavior of sequence files (e.g. `TFRecords`) by reading a group of examples together.
 
-## Create your own benchmark
-TODO
+## Datasets
+The following datasets are included in this suite:
+* AirFreight
+* COCO
+* berkeley_segmentation
+* flickr30k
+* flickr8k
+* google_house_number
+* imagenet
+* youtube-8m-frame
+* youtube-8m-video
+ 
+# Notes
+* Make sure the data is not cached in memory before running any benchmark. To flush the cache run the following as `root`:
+```bash
+free && sync && echo 3 > /proc/sys/vm/drop_caches && free
+```
+* Make the total data size is large enough (at least 10 times the size of memory) while still fit on the disk. By default, this value is `256GB`. If you need to change this value, generate new benchmarks by running:
+```bash
+python3 dio/generate_fio.py --total_size=256g
+``` 
+
+## Add a Custom Workload
+1. Add a stat file to `dataset-stats` folder. The name of the file should be in form of `stat_DATASET.json` where DATASET is the name of the dataset. See `dataset-stats/stat_imagenet.json` to learn about the structure of stats files.
+2. generate new benchmarks by running:
+```bash
+python3 dio/generate_fio.py
+``` 
+
+## Advance Options
+```bash
+python3 dio/generate_fio.py -h
+usage: generate_fio.py [-h] [--total_size TOTAL_SIZE]
+                       [--depths DEPTHS [DEPTHS ...]] [--seqs SEQS [SEQS ...]]
+                       [--sizes SIZES [SIZES ...]]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --total_size TOTAL_SIZE
+                        Total data size
+  --depths DEPTHS [DEPTHS ...]
+                        List of IO depth sizes.
+  --seqs SEQS [SEQS ...]
+                        List of sequence file sizes in dataset workloads.
+  --sizes SIZES [SIZES ...]
+                        List of fixed sizes in general workloads.
+
+```
--- dio/generate_fio.py
+++ dio/generate_fio.py
@@ -1,13 +1,12 @@
 #! /usr/bin/env python -u
 # coding=utf-8
+import argparse
 import json
 import os
 import re
 from collections import OrderedDict
 from configparser import ConfigParser
-import seaborn as sns
 import numpy as np
-import matplotlib.pyplot as plt
 import random
 
 __author__ = 'Sayed Hadi Hashemi'
@@ -39,11 +38,15 @@ class FioRunBase:
         self._add_section(config, "global", self.global_configurations)
         for name, test in self.tests.items():
             self._add_section(config, name, test)
-        with open('experiments/%s.fio' % self.name, 'w') as configfile:
+        file_name = 'experiments/%s.fio' % self.name
+        if not os.path.exists(os.path.dirname(file_name)):
+            os.mkdir(os.path.dirname(file_name))
+        with open(file_name, 'w') as configfile:
             config.write(configfile, space_around_delimiters=False)
 
 
 class FioRun(FioRunBase):
+    total_size = "256g"
     global_configurations = {
         "directory": "${DEVICE}",
         "filename_format": "data-$filenum.bin",
@@ -77,50 +80,68 @@ class FioRun(FioRunBase):
 
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
+        self.global_configurations["filesize"] = self.total_size
+        self.global_configurations["size"] = self.total_size
 
-    def add_sync(self, name, stonewall=True, depth=1, **kwargs):
+    def _add_sync(self, name, stonewall=True, depth=1, **kwargs):
         experiment = self.sync_configurations.copy()
         experiment["numjobs"] = depth
         experiment.update(kwargs)
         self.add_test(name, stonewall, **experiment)
 
-    def add_async(self, name, stonewall=True, depth=1, **kwargs):
+    def _add_async(self, name, stonewall=True, depth=1, **kwargs):
         experiment = self.async_configurations.copy()
         experiment["iodepth"] = depth
         experiment.update(kwargs)
         self.add_test(name, stonewall, **experiment)
 
+    def add(self, name, api, stonewall=True, depth=1, **kwargs):
+        if api == "sync":
+            kwargs["direct"] = 0
+            return self._add_sync(name, stonewall, depth, **kwargs)
+        elif api == "sync-direct":
+            kwargs["direct"] = 1
+            return self._add_sync(name, stonewall, depth, **kwargs)
+        elif api == "async":
+            kwargs["direct"] = 1
+            return self._add_async(name, stonewall, depth, **kwargs)
+        elif api == "async-indirect":
+            kwargs["direct"] = 0
+            return self._add_async(name, stonewall, depth, **kwargs)
+        else:
+            raise Exception("API is not found: {}".format(api))
+
 
 class FioGeneralExperiment:
-    depths = [1, 8, 32, 512]
+    depths = [1, 8, 32, 128]
     sizes = [64, 256, 1024, 4096, 32768, 262144, 2 ** 32]
-    variations = ["sync", "async"]
+    apis = ["sync", "async", "sync-direct", "async-indirect"]
 
     def __init__(self) -> None:
         super().__init__()
 
     def generate_experiment(self):
         runs = []
-        for var in self.variations:
-            name = "general-{}".format(var)
+        for api in self.apis:
+            name = "{}/general".format(api)
             run = FioRun(name=name)
 
             for size in self.sizes:
                 for depth in self.depths:
-                    func = run.add_sync if var == "sync" else run.add_async
-                    func("{var}-d{depth}-s{size}".format(var=var, depth=depth, size=size),
-                         depth=depth,
-                         bssplit="{}/1".format(size)
-                         )
+                    run.add("{api}-d{depth}-s{size}".format(api=api, depth=depth, size=size),
+                            api=api,
+                            depth=depth,
+                            bssplit="{}/1".format(size)
+                            )
             run.generate_config_file()
             print(run.name)
             runs.append(run)
 
 
 class FioDatasetExperiment:
-    depths = [1, 2, 8, 32, 512]
+    depths = [1, 2, 8, 32, 128]
     sequence_sizes = [1, 32, 256]
-    variations = ["sync", "async"]
+    apis = ["sync", "async", "sync-direct", "async-indirect"]
 
     def __init__(self, dataset_name, dataset_file=None) -> None:
         super().__init__()
@@ -170,17 +191,17 @@ class FioDatasetExperiment:
 
     def generate_experiment(self):
         runs = []
-        for var in self.variations:
-            name = "{}-{}".format(self.dataset_name, var)
+        for api in self.apis:
+            name = "{}/{}".format(api, self.dataset_name)
             run = FioRun(name=name)
-            func = run.add_sync if var == "sync" else run.add_async
 
             for sequence_size in self.sequence_sizes:
                 mix = self.get_dataset_mix(sequence_size)
                 for mix_name, dist in mix.items():
                     for depth in self.depths:
-                        func("{var}-{mix}-s{sequence_size}-d{depth}".format(
-                            mix=mix_name, var=var, depth=depth, sequence_size=sequence_size),
+                        run.add("{api}-{mix}-s{sequence_size}-d{depth}".format(
+                            mix=mix_name, api=api, depth=depth, sequence_size=sequence_size),
+                            api=api,
                             depth=depth,
                             bssplit=self.get_bssplit(dist)
                         )
@@ -198,7 +219,61 @@ class FioDatasetExperiment:
         return ret
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--total_size",
+        type=str,
+        help="Total data size",
+        default="256g",
+        required=False
+    )
+
+    parser.add_argument(
+        "--depths",
+        type=int,
+        nargs='+',
+        help="List of IO depth sizes.",
+        default=[1, 2, 8, 32, 128],
+        required=False
+    )
+
+    parser.add_argument(
+        "--seqs",
+        type=int,
+        nargs='+',
+        help="List of sequence file sizes in dataset workloads.",
+        default=[1, 32, 256],
+        required=False
+    )
+
+    parser.add_argument(
+        "--sizes",
+        type=int,
+        nargs='+',
+        help="List of fixed sizes in general workloads.",
+        default=[64, 256, 1024, 4096, 32768, 262144, 2 ** 32],
+        required=False
+    )
+
+    FLAGS, unparsed = parser.parse_known_args()
+
+    if unparsed:
+        print("Unknown argument:", unparsed)
+        exit(1)
+
+    FioRun.total_size = FLAGS.total_size
+
+    FioGeneralExperiment.depths = FLAGS.depths
+    FioGeneralExperiment.sizes = FLAGS.sizes
+
+    FioDatasetExperiment.depths = FLAGS.depths
+    FioDatasetExperiment.sequence_sizes = FLAGS.seqs
+
+
 if __name__ == '__main__':
+    parse_args()
+
     FioGeneralExperiment().generate_experiment()
     for dataset in FioDatasetExperiment.list_datasets():
         FioDatasetExperiment(dataset_name=dataset).generate_experiment()
